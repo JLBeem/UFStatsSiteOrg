9a9bb782e7943725d78e466f730bcf712a0bbbba
bobbobkilu <bobbobkillu@hotmail.com>
2021-03-22 17:29:25 -0400

    Implemented google sheet embed iframe

--- react-app/src/components/internships/InternshipsPage.jsx
+++ react-app/src/components/internships/InternshipsPage.jsx
@@ -1,8 +1,9 @@
 function InternshipsPage() {
     return (
         <div>
-            InternshipsPage
-        </div>
+            <h1>Internship Page</h1>
+            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT_XeXB6si2seOZNBZOHd2TFScmjOWU1QeUgK4niRBXxvLNch7VztI83HgPaZXwRxHO6tlYo-TGDNFF/pubhtml?widget=true&amp;headers=false" width="80%" height="300"></iframe>
+        </div>     
     )
 }
 
